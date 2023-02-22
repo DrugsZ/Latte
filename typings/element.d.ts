82c0961fd340ec34d37bc4e16a22e826b0e08130
FateLee <z1033849617@gmail.com>
2023-02-22 23:39:02 +0800

    fix(global): change type name to fix repeat to document

--- typings/element.d.ts
+++ typings/element.d.ts
@@ -1,57 +1,70 @@
-type ElementType = "FRAME" | "TEXT" | "GROUP" | "RECTANGLE" | 'Vector' | 'LINE' | 'ARROW' | 'ELLIPSE' | 'POLYGON' | 'STAR'
+type ElementType =
+    | 'FRAME'
+    | 'TEXT'
+    | 'GROUP'
+    | 'RECTANGLE'
+    | 'Vector'
+    | 'LINE'
+    | 'ARROW'
+    | 'ELLIPSE'
+    | 'POLYGON'
+    | 'STAR'
 
 interface BaseFill {
-  type: string
-  visible:boolean
-  opacity:number
+    type: string
+    visible: boolean
+    opacity: number
 }
 
 interface SolidColorFill extends BaseFill {
-  type:"SOLID"
-  color:{
-    r:number
-    g:number
-    b:number
-  }
+    type: 'SOLID'
+    color: {
+        r: number
+        g: number
+        b: number
+    }
 }
 
 type Fill = SolidColorFill
 
-type Transform = [number,number,number,number,number,number]
+type Transform = [number, number, number, number, number, number]
 
 interface BaseNode {
-  id:string
-  type: string,
-	name: string
+    id: string
+    type: string
+    name: string
 }
 
-interface Document extends BaseNode {
-  type: "DOCUMENT",
-  children: PAGE[]
+interface CditorDocument extends BaseNode {
+    type: 'DOCUMENT'
+    children: PAGE[]
 }
 
 interface PAGE extends BaseNode {
-  type: "PAGE"
-  backgrounds:Fill[]
-  children: Element[]
+    type: 'PAGE'
+    backgrounds: Fill[]
+    children: CditorElement[]
 }
 
-interface Element extends BaseNode {
-  type:ElementType
-  hidden:boolean
-  locked:boolean
-  x:number
-  y:number
-  width:number
-  height:number
-  relativeTransform:Transform
-  absoluteTransform?:Transform
-  opacity:boolean
-  fills:Fill[]
+interface CditorElement extends BaseNode {
+    type: ElementType
+    hidden: boolean
+    locked: boolean
+    x: number
+    y: number
+    width: number
+    height: number
+    relativeTransform: Transform
+    absoluteTransform?: Transform
+    opacity: number
+    fills: Fill[]
 }
 
 interface RectangleElement extends Element {
-  type:"RECTANGLE"
-  radius:[number,number,number,number]
+    type: 'RECTANGLE'
+    radius: [number, number, number, number]
 }
 
+interface Window {
+    test: any
+}
