8ed54e927afda5f1973d19988defd4e233c9c168
FateLee <z1033849617@gmail.com>
2023-03-07 03:48:56 +0000

    chore: turn off some useless message

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackBar = require('webpackbar');
+const FriendlyErrorsWebpackPlugin = require('@soda/friendly-errors-webpack-plugin');
 
 
 module.exports = {
@@ -9,11 +10,16 @@ module.exports = {
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
-    hot: true,
+    hot: true
+  },
+  infrastructureLogging: {
+    level: 'error',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'cditor.bundle.js',
+    clean: true,
+    publicPath: '/',
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js"],
@@ -39,8 +45,15 @@ module.exports = {
       }
     ]
   },
+  stats: 'errors-only',
   plugins: [
     new HtmlWebpackPlugin(),
-    new WebpackBar()
+    new WebpackBar(),
+    new FriendlyErrorsWebpackPlugin({
+      compilationSuccessInfo: {
+        messages: ['You application is running here http://localhost:8080'],
+        notes: ['Some additional notes to be displayed upon successful compilation']
+      },
+    })
   ],
 }
