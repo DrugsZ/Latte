2d08efa103b4dd03f56556764a0f4ae15cbc5e54
FateLee <z1033849617@gmail.com>
2023-06-16 17:36:26 +0800

    feat: canvas cover all body

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import Editor from 'Cditor/core/editor'
 
-const initCanvas = () => {
+const initCanvas = ({ width = 800, height = 800 }) => {
   const canvas = document.createElement('canvas')
-  canvas.setAttribute('width', '800')
-  canvas.setAttribute('height', '800')
-  canvas.style.width = '800px'
-  canvas.style.height = '800px'
+  canvas.setAttribute('width', String(width))
+  canvas.setAttribute('height', String(height))
+  canvas.style.width = `${width}px`
+  canvas.style.height = `${height}px`
   return canvas
 }
-
-const canvas = initCanvas()
+document.body.style.height = '100vh'
+document.body.style.width = '100vw'
+const bodyRect = document.body.getBoundingClientRect()
+const canvas = initCanvas(bodyRect)
 document.body.appendChild(canvas)
 const test = new Editor(canvas)
 window.test = test
