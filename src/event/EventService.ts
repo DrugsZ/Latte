40821695efab7cbd5ff913016398a5c2de7a62cb
FateLee <z1033849617@gmail.com>
2023-07-02 01:19:31 +0800

    feat: complete EventService

--- src/event/EventService.ts
+++ src/event/EventService.ts
@@ -8,6 +8,18 @@ import { Point } from 'Latte/math/Point'
 import { isDisplayObject } from 'Latte/utils/assert'
 
 type Picker = (event: Point) => IEventTarget | null
+type TrackingData = {
+  pressTargetsByButton: Record<number, IEventTarget[]>
+  clicksByButton: Record<
+    number,
+    {
+      clickCount: number
+      target: IEventTarget
+      timeStamp: number
+    }
+  >
+  overTargets: IEventTarget[] | null
+}
 
 const PROPAGATION_LIMIT = 2048
 
@@ -23,6 +35,10 @@ export class EventService {
   private _eventPool: Map<typeof FederatedEvent, FederatedEvent[]> = new Map()
 
   private _pickHandler: Picker
+
+  private _mappingState: Record<string, any> = {
+    trackingData: {},
+  }
 
   constructor(private _rootTarget: IEventTarget) {
     this._init()
@@ -48,9 +64,9 @@ export class EventService {
   }
 
   mapEvent(e: FederatedEvent) {
-    // if (!this.rootTarget) {
-    //   return
-    // }
+    if (!this._rootTarget) {
+      return
+    }
 
     const mappers = this._mappingTable[e.type]
 
@@ -65,7 +81,6 @@ export class EventService {
 
   private _init() {
     this._addEventMapping('pointerdown', this._onPointerDown)
-    this._addEventMapping('mousedown', this._onPointerDown)
     this._addEventMapping('pointerup', this._onPointerUp)
     this._addEventMapping('pointermove', this._onPointerMove)
     this._addEventMapping('pointerout', this._onPointerOut)
@@ -73,6 +88,26 @@ export class EventService {
     this._addEventMapping('pointerover', this._onPointerOver)
     this._addEventMapping('pointerupoutside', this._onPointerUpOutside)
     this._addEventMapping('wheel', this._onWheel)
+  }
+
+  clonePointerEvent(
+    from: FederatedPointerEvent,
+    type?: string
+  ): FederatedPointerEvent {
+    const event = this._allocateEvent(FederatedPointerEvent)
+
+    event.nativeEvent = from.nativeEvent
+    event.originalEvent = from.originalEvent
+
+    EventService.copyPointerData(from, event)
+    EventService.copyMouseData(from, event)
+    EventService.copyData(from, event)
+
+    event.target = from.target
+    event.path = from.composedPath().slice()
+    event.type = type ?? event.type
+
+    return event
   }
 
   static copyPointerData(from: FederatedEvent, to: FederatedEvent) {
@@ -135,7 +170,7 @@ export class EventService {
     to.viewport.copyFrom(from.viewport)
   }
 
-  private allocateEvent<T extends FederatedEvent>(constructor: {
+  private _allocateEvent<T extends FederatedEvent>(constructor: {
     new (boundary: EventService): T
   }): T {
     if (!this._eventPool.has(constructor as any)) {
@@ -155,7 +190,7 @@ export class EventService {
     return event
   }
 
-  private freeEvent<T extends FederatedEvent>(event: T) {
+  private _freeEvent<T extends FederatedEvent>(event: T) {
     if (event.manager !== this)
       throw new Error(
         'It is illegal to free an event not managed by this EventBoundary!'
@@ -171,21 +206,21 @@ export class EventService {
     this._eventPool.get(constructor as any).push(event)
   }
 
-  private notifyTarget(e: FederatedEvent, type?: string) {
+  private _notifyTarget(e: FederatedEvent, type?: string) {
     type = type ?? e.type
     const key =
       e.eventPhase === e.CAPTURING_PHASE || e.eventPhase === e.AT_TARGET
         ? `${type}capture`
         : type
 
-    this.notifyListeners(e, key)
+    this._notifyListeners(e, key)
 
     if (e.eventPhase === e.AT_TARGET) {
-      this.notifyListeners(e, type)
+      this._notifyListeners(e, type)
     }
   }
 
-  private notifyListeners(e: FederatedEvent, type: string) {
+  private _notifyListeners(e: FederatedEvent, type: string) {
     // @ts-ignore
     const emitter = e.currentTarget.emitter
     // @ts-ignore
@@ -217,7 +252,7 @@ export class EventService {
     type?: string,
     target?: IEventTarget
   ): Promise<FederatedPointerEvent> {
-    const event = this.allocateEvent(FederatedPointerEvent)
+    const event = this._allocateEvent(FederatedPointerEvent)
 
     EventService.copyPointerData(from, event)
     EventService.copyMouseData(from, event)
@@ -237,7 +272,7 @@ export class EventService {
   private async _createWheelEvent(
     from: FederatedWheelEvent
   ): Promise<FederatedWheelEvent> {
-    const event = this.allocateEvent(FederatedWheelEvent)
+    const event = this._allocateEvent(FederatedWheelEvent)
 
     EventService.copyWheelData(from, event)
     EventService.copyMouseData(from, event)
@@ -258,7 +293,7 @@ export class EventService {
       e.eventPhase = e.AT_TARGET
       const canvas = this._rootTarget || null
       e.currentTarget = canvas
-      this.notifyListeners(e, type)
+      this._notifyListeners(e, type)
     }
   }
 
@@ -276,14 +311,14 @@ export class EventService {
     e.eventPhase = e.CAPTURING_PHASE
     for (let i = composedPath.length - 1; i >= 1; i--) {
       e.currentTarget = composedPath[i]
-      this.notifyTarget(e, type)
+      this._notifyTarget(e, type)
       if (e.propagationStopped || e.propagationImmediatelyStopped) return
     }
 
     // target phase
     e.eventPhase = e.AT_TARGET
     e.currentTarget = e.target
-    this.notifyTarget(e, type)
+    this._notifyTarget(e, type)
     if (e.propagationStopped || e.propagationImmediatelyStopped) return
 
     // find current target in composed path
@@ -293,7 +328,7 @@ export class EventService {
     e.eventPhase = e.BUBBLING_PHASE
     for (let i = index + 1; i < composedPath.length; i++) {
       e.currentTarget = composedPath[i]
-      this.notifyTarget(e, type)
+      this._notifyTarget(e, type)
       if (e.propagationStopped || e.propagationImmediatelyStopped) return
     }
   }
@@ -314,6 +349,41 @@ export class EventService {
     return propagationPath
   }
 
+  private _trackingData(id: number): TrackingData {
+    if (!this._mappingState.trackingData[id]) {
+      this._mappingState.trackingData[id] = {
+        pressTargetsByButton: {},
+        clicksByButton: {},
+        overTarget: null,
+      }
+    }
+
+    return this._mappingState.trackingData[id]
+  }
+
+  private _findMountedTarget(
+    propagationPath: IEventTarget[] | null
+  ): IEventTarget | null {
+    if (!propagationPath) {
+      return null
+    }
+
+    let currentTarget = propagationPath[propagationPath.length - 1]
+    for (let i = propagationPath.length - 2; i >= 0; i--) {
+      const target = propagationPath[i]
+      if (
+        target === this._rootTarget ||
+        (isDisplayObject(target) && target.parentNode === currentTarget)
+      ) {
+        currentTarget = propagationPath[i]
+      } else {
+        break
+      }
+    }
+
+    return currentTarget
+  }
+
   private _onPointerDown = async (from: FederatedEvent) => {
     if (!(from instanceof FederatedPointerEvent)) {
       return
@@ -326,7 +396,10 @@ export class EventService {
 
       this.dispatchEvent(e, isRightButton ? 'rightdown' : 'mousedown')
     }
-    this.freeEvent(e)
+    const trackingData = this._trackingData(from.pointerId)
+
+    trackingData.pressTargetsByButton[from.button] = e.composedPath()
+    this._freeEvent(e)
   }
 
   private _onPointerMove = async (from: FederatedEvent) => {
@@ -342,7 +415,7 @@ export class EventService {
 
       this.dispatchEvent(e, isRightButton ? 'rightmove' : 'mousemove')
     }
-    this.freeEvent(e)
+    this._freeEvent(e)
   }
 
   private _onPointerUp = async (from: FederatedEvent) => {
@@ -350,6 +423,7 @@ export class EventService {
       return
     }
 
+    const now = performance.now()
     const e = await this._createPointerEvent(from)
     this.dispatchEvent(e, 'pointerup')
     if (e.pointerType === 'mouse') {
@@ -357,47 +431,234 @@ export class EventService {
 
       this.dispatchEvent(e, isRightButton ? 'rightup' : 'mouseup')
     }
-    this.freeEvent(e)
+    const trackingData = this._trackingData(from.pointerId)
+    const pressTarget = this._findMountedTarget(
+      trackingData.pressTargetsByButton[from.button]
+    )
+
+    let clickTarget = pressTarget
+
+    // pointerupoutside only bubbles. It only bubbles upto the parent that doesn't contain
+    // the pointerup location.
+    if (pressTarget && !e.composedPath().includes(pressTarget)) {
+      let currentTarget: IEventTarget | null = pressTarget
+
+      while (currentTarget && !e.composedPath().includes(currentTarget)) {
+        e.currentTarget = currentTarget
+
+        this._notifyTarget(e, 'pointerupoutside')
+
+        if (e.pointerType === 'touch') {
+          this._notifyTarget(e, 'touchendoutside')
+        } else if (e.pointerType === 'mouse' || e.pointerType === 'pen') {
+          const isRightButton = e.button === 2
+
+          this._notifyTarget(
+            e,
+            isRightButton ? 'rightupoutside' : 'mouseupoutside'
+          )
+        }
+
+        if (isDisplayObject(currentTarget)) {
+          currentTarget = currentTarget.parentNode
+        }
+      }
+
+      delete trackingData.pressTargetsByButton[from.button]
+
+      // currentTarget is the most specific ancestor holding both the pointerdown and pointerup
+      // targets. That is - it's our click target!
+      clickTarget = currentTarget
+    }
+
+    if (clickTarget) {
+      const clickEvent = this.clonePointerEvent(e, 'click')
+
+      clickEvent.target = clickTarget
+      clickEvent.path = []
+
+      if (!trackingData.clicksByButton[from.button]) {
+        trackingData.clicksByButton[from.button] = {
+          clickCount: 0,
+          target: clickEvent.target,
+          timeStamp: now,
+        }
+      }
+
+      const clickHistory = trackingData.clicksByButton[from.button]
+
+      if (
+        clickHistory.target === clickEvent.target &&
+        now - clickHistory.timeStamp < 200
+      ) {
+        ++clickHistory.clickCount
+      } else {
+        clickHistory.clickCount = 1
+      }
+
+      clickHistory.target = clickEvent.target
+      clickHistory.timeStamp = now
+
+      clickEvent.detail = clickHistory.clickCount
+
+      // @see https://github.com/antvis/G/issues/1091
+      if (!e.detail?.preventClick) {
+        if (
+          clickEvent.pointerType === 'mouse' ||
+          clickEvent.pointerType === 'touch'
+        ) {
+          this.dispatchEvent(clickEvent, 'click')
+        }
+        this.dispatchEvent(clickEvent, 'pointertap')
+      }
+
+      this._freeEvent(clickEvent)
+    }
+
+    this._freeEvent(e)
   }
 
   private _onPointerOut = async (from: FederatedEvent) => {
-    console.log(from)
     if (!(from instanceof FederatedPointerEvent)) {
       return
     }
+    const trackingData = this._trackingData(from.pointerId)
 
-    const now = performance.now()
-    const e = await this._createPointerEvent(from)
-    this.freeEvent(e)
+    if (trackingData.overTargets) {
+      const isMouse = from.pointerType === 'mouse' || from.pointerType === 'pen'
+      const outTarget = this._findMountedTarget(trackingData.overTargets)
+
+      // pointerout first
+      const outEvent = await this._createPointerEvent(
+        from,
+        'pointerout',
+        outTarget || undefined
+      )
+
+      this.dispatchEvent(outEvent, 'pointerout')
+      if (isMouse) this.dispatchEvent(outEvent, 'mouseout')
+
+      // pointerleave(s) are also dispatched b/c the pointer must've left rootTarget and its descendants to
+      // get an upstream pointerout event (upstream events do not know rootTarget has descendants).
+      const leaveEvent = await this._createPointerEvent(
+        from,
+        'pointerleave',
+        outTarget || undefined
+      )
+
+      leaveEvent.eventPhase = leaveEvent.AT_TARGET
+
+      while (
+        leaveEvent.target &&
+        leaveEvent.target !==
+          (isDisplayObject(this._rootTarget) && this._rootTarget.parentNode)
+      ) {
+        leaveEvent.currentTarget = leaveEvent.target
+
+        this._notifyTarget(leaveEvent)
+        if (isMouse) {
+          this._notifyTarget(leaveEvent, 'mouseleave')
+        }
+
+        if (isDisplayObject(leaveEvent.target)) {
+          leaveEvent.target = leaveEvent.target.parentNode
+        }
+      }
+
+      trackingData.overTargets = null
+
+      this._freeEvent(outEvent)
+      this._freeEvent(leaveEvent)
+    }
   }
   private _onPointerOver = async (from: FederatedEvent) => {
-    console.log(from)
     if (!(from instanceof FederatedPointerEvent)) {
       return
     }
 
-    const now = performance.now()
+    const trackingData = this._trackingData(from.pointerId)
     const e = await this._createPointerEvent(from)
-    this.freeEvent(e)
+
+    const isMouse = e.pointerType === 'mouse' || e.pointerType === 'pen'
+
+    this.dispatchEvent(e, 'pointerover')
+    if (isMouse) this.dispatchEvent(e, 'mouseover')
+
+    // pointerenter events must be fired since the pointer entered from upstream.
+    const enterEvent = this.clonePointerEvent(e, 'pointerenter')
+
+    enterEvent.eventPhase = enterEvent.AT_TARGET
+
+    while (
+      enterEvent.target &&
+      enterEvent.target !==
+        (isDisplayObject(this._rootTarget) && this._rootTarget.parentNode)
+    ) {
+      enterEvent.currentTarget = enterEvent.target
+
+      this._notifyTarget(enterEvent)
+      if (isMouse) {
+        // mouseenter should not bubble
+        // @see https://developer.mozilla.org/en-US/docs/Web/API/Element/mouseenter_event#usage_notes
+        this._notifyTarget(enterEvent, 'mouseenter')
+      }
+
+      if (isDisplayObject(enterEvent.target)) {
+        enterEvent.target = enterEvent.target.parentNode
+      }
+    }
+
+    trackingData.overTargets = e.composedPath()
+
+    this._freeEvent(e)
+    this._freeEvent(enterEvent)
   }
   private _onPointerUpOutside = async (from: FederatedEvent) => {
-    console.log(from)
     if (!(from instanceof FederatedPointerEvent)) {
       return
     }
 
-    const now = performance.now()
+    const trackingData = this._trackingData(from.pointerId)
+    const pressTarget = this._findMountedTarget(
+      trackingData.pressTargetsByButton[from.button]
+    )
     const e = await this._createPointerEvent(from)
-    this.freeEvent(e)
+
+    if (pressTarget) {
+      let currentTarget: IEventTarget | null = pressTarget
+
+      while (currentTarget) {
+        e.currentTarget = currentTarget
+
+        this._notifyTarget(e, 'pointerupoutside')
+
+        if (e.pointerType === 'touch') {
+          // this.notifyTarget(e, 'touchendoutside');
+        } else if (e.pointerType === 'mouse' || e.pointerType === 'pen') {
+          this._notifyTarget(
+            e,
+            e.button === 2 ? 'rightupoutside' : 'mouseupoutside'
+          )
+        }
+
+        if (isDisplayObject(currentTarget)) {
+          currentTarget = currentTarget.parentNode
+        }
+      }
+
+      delete trackingData.pressTargetsByButton[from.button]
+    }
+
+    this._freeEvent(e)
   }
   private _onWheel = async (from: FederatedEvent) => {
-    console.log(from)
     if (!(from instanceof FederatedWheelEvent)) {
       return
     }
 
-    const now = performance.now()
-    const e = await this._createWheelEvent(from)
-    this.freeEvent(e)
+    const wheelEvent = await this._createWheelEvent(from)
+
+    this.dispatchEvent(wheelEvent, 'wheel')
+    this._freeEvent(wheelEvent)
   }
 }
