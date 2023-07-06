5cb74dd3df18ec4928981e76c455ac47ad0b68c7
FateLee <z1033849617@gmail.com>
2023-07-07 01:50:22 +0800

    feat: add some ASCII math method

--- src/math/zIndex.ts
+++ src/math/zIndex.ts
@@ -1,3 +1,6 @@
+const MIN_ASCII = '!'
+const MAX_ASCII = '}'
+
 export const compareASCII = (a: string, b: string) => {
   const length = Math.max(a.length, b.length)
   for (let i = 0; i < length; i++) {
@@ -8,10 +11,46 @@ export const compareASCII = (a: string, b: string) => {
       return true
     }
     if (a[i] === b[i]) {
+      // eslint-disable-next-line no-continue
       continue
     }
     const aNumber = a.charCodeAt(i)
     const bNumber = b.charCodeAt(i)
     return aNumber >= bNumber
   }
+  return false
 }
+
+export function decreaseOne(a: string) {
+  if (a.length > 1) {
+    const str = a.split('')
+    str[str.length - 1] = decreaseOne(a.charAt(a.length - 1))
+    return str.join('')
+  }
+  if (a === MIN_ASCII) {
+    return ` }`
+  }
+  return String.fromCodePoint(a.charCodeAt(0) - 1)
+}
+
+export function plusOne(a: string) {
+  if (a.length > 1) {
+    const str = a.split('')
+    str[str.length - 1] = plusOne(a.charAt(a.length - 1))
+    return str.join('')
+  }
+  if (a === MAX_ASCII) {
+    return `}!`
+  }
+  return String.fromCodePoint(a.charCodeAt(0) - 1)
+}
+
+// export function getNewASCIIByTowValue(value1: null, value2: string)
+// export function getNewASCIIByTowValue(value1: string, value2: null)
+// export function getNewASCIIByTowValue(value1: string, value2: string) {
+//   if (!value1) {
+//     return
+//   }
+//   if (!value2) {
+//   }
+// }
