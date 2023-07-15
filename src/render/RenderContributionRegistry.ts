f0fdf65e713ce6335d8f54634b63e2ddb7ed0715
FateLee <z1033849617@gmail.com>
2023-07-16 00:18:56 +0800

    type: fix contextMatrix type

--- src/render/RenderContributionRegistry.ts
+++ src/render/RenderContributionRegistry.ts
@@ -1,6 +1,7 @@
 import type { FillType } from 'Latte/constants/schema'
 import type Rect from 'Latte/elements/Rect'
 import type Ellipse from 'Latte/elements/Ellipse'
+import type { Matrix } from 'Latte/math/matrix'
 
 export interface EditorTypeRenderCtor<T> {
   new (): T
@@ -85,7 +86,7 @@ export interface IEditorShapeRenderContributionDescription<
   readonly render: (
     renderObject: ShapeRenderTypeMaps[T],
     ctx: CanvasRenderingContext2D,
-    contextMatrix: Float32Array
+    contextMatrix: Matrix
   ) => void
 }
 
